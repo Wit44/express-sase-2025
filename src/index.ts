55f1bf06b15e21b1476eda18be3fd8218c446c86
Vladimir Milosavljevic <vladimir.milosavljevic.21@singimail.rs>
2025-03-31 15:39:31 +0200

    Installed dependencies

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log('1')
+import express from 'express'
+import cors from 'cors'
+import morgan from 'morgan'
+
+const app = express()
+app.use(cors())
+app.use(morgan('tiny'))
+
+app.get('/', (req, res) => {
+    res.send('hi')
+})
+
+app.listen(3000, () => console.log('app started'))
