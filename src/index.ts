bf01ebe564f5cef3a6089511d0002c65ff6e2b19
Vladimir Milosavljevic <vladimir.milosavljevic.21@singimail.rs>
2025-04-02 16:10:53 +0200

    Added database

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,16 @@ import cors from 'cors'
 import morgan from 'morgan'
 import { FlightRoute } from './routes/flight.route'
 import { configDotenv } from 'dotenv'
-import { Timestamp } from 'typeorm'
+import { AppDataSource } from './db'
+import { AirlineRoute } from './routes/airline.route'
+
 
 const app = express()
 app.use(cors())
 app.use(morgan('tiny'))
 
 app.use('/api/flight', FlightRoute)
+app.use('/api/airline', AirlineRoute)
 
 app.get('*', (req, res) => {
     res.status(404).json({
@@ -19,5 +22,11 @@ app.get('*', (req, res) => {
 })
 
 configDotenv()
-const port = process.env.SERVER_PORT || 3000
-app.listen(port, () => console.log(`App started on port ${port}`))
+AppDataSource.initialize().then(() => {
+    const port = process.env.SERVER_PORT || 3000
+    app.listen(port, () => console.log(`App started on port ${port}`))
+})
+.catch(e => {
+    console.log('Database server connection failed')
+    console.log(e)
+})
